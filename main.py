674b4e0859debe41c42a68cbcbdeac0d1c2e830f
Lucas <87275856+luvrgz@users.noreply.github.com>
2022-10-21 12:51:33 +0200

    html et route crées

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 
 list_group = [Group.client]
 dict_group = dict()
-for group in list_group:
-	dict_group[group.name] = group
+for tmp_group in list_group:
+	dict_group[tmp_group.name] = tmp_group
 
+print(dict_group)
 # ---------------------------------------
 # les différentes pages (fonctions VUES)
 # ---------------------------------------
@@ -20,16 +21,19 @@ for group in list_group:
 
 @app.route('/')
 def index():
-	print(list(dict_group.keys()))
-	return render_template('home.html', group_names=list(dict_group.keys()))
+	list_group_names = list(dict_group.keys())
+	return render_template('home.html', group_names=list_group_names)
 
 
-@app.route('/<group>')
-def group_homepage(group_name=None):
-	return dict_group[group_name]
+@app.route('/homepage')
+def group_homepage():
+	group_name = request.args.get('group')
+	group = dict_group[group_name]
+	return group.name
 
-@app.route('/afficher_personnes', methods=['GET'])
-def afficher_personnes():
+
+@app.route('/afficher_commandes', methods=['GET'])
+def afficher_commandes():
 	
 	con = lite.connect('exemples.db')
 	con.row_factory = lite.Row
@@ -37,29 +41,34 @@ def afficher_personnes():
 	cur.execute("SELECT nom, prenom, role FROM personnes")
 	lignes = cur.fetchall()
 	con.close()
-	return render_template('affichage_personnes.html', personnes = lignes)
+	return render_template('afficher_commandes.html', personnes = lignes)
 	
-@app.route('/ajouter_personne', methods=['GET', 'POST'])
-def ajouter_personne():
-	
+@app.route('/passer_commande', methods=['GET', 'POST'])
+def passer_commande():
+	group_name = request.args.get('group')
+
 	if not request.method == 'POST':
-		return render_template('formulaire_personne.html', msg = "", nom = "", prenom = "", role = 0)
+		return render_template('passer_commande.html', msg = "", nom = "", prenom = "", role = 0, group=group_name)
 	else:
 		nom = request.form.get('nom', '')
 		prenom = request.form.get('prenom','')
 		role = request.form.get('role', 0, type=int)
 		
 		if (nom != "" and prenom != "" and role > 0 and role < 4):
-			con = lite.connect('exemples.db')
-			con.row_factory = lite.Row
-			cur = con.cursor()
-			cur.execute("INSERT INTO personnes('nom', 'prenom', 'role') VALUES (?,?,?)", (nom,prenom,role))
-			con.commit()
-			con.close()
-			return redirect(url_for('afficher_personnes'))
+			# con = lite.connect('exemples.db')
+			# con.row_factory = lite.Row
+			# cur = con.cursor()
+			# cur.execute("INSERT INTO personnes('nom', 'prenom', 'role') VALUES (?,?,?)", (nom,prenom,role))
+			# con.commit()
+			# con.close()
+			return redirect(url_for('succes'))
 		else:
-			return render_template('formulaire_personne.html', msg = "Mauvaise saisie !", nom = "", prenom = "", role = 0)
+			return render_template('passer_commande.html', msg = "Mauvaise saisie !", nom = "", prenom = "", role = 0, group=group_name)
 
+@app.route('/succes')
+def succes():
+	group_name = request.args.get('group')
+	return "succes"
 
 # ---------------------------------------
 # pour lancer le serveur web local Flask
